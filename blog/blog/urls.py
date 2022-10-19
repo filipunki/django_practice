0ef874ac9c14c5d163481589be689a7367ccae0b
filipunki <filipunki@gmail.com>
2022-10-19 15:03:16 +0200

    created a new post page and an edit page

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path
-from .views import HomePageView, BlogDetailView
+from .views import HomePageView, BlogDetailView, BlogCreateView, BlogUpdateView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="blog-home"),
-    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail")
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),
+    path("post/<int:pk>/edit", BlogUpdateView.as_view(), name="post_edit"),
     
 ]
--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Post
 
 class HomePageView(ListView):
@@ -10,5 +11,14 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
 
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author","body"]
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
 
 # Create your views here.
