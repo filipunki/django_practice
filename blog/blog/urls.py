02b9a407fbdb2d9d9dd4eb6ceb12a9689ffe2406
filipunki <filipunki@gmail.com>
2022-10-19 14:29:53 +0200

    made some tests

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, BlogDetailView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="blog-home"),
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail")
     
 ]
--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class HomePageView(ListView):
     model = Post
     template_name = "home.html"
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
 
 
 # Create your views here.
